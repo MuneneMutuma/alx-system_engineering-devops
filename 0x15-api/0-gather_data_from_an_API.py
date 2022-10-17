7362b9ef8155026af0717190f0a2795cfd81a3c0
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-10-17 13:38:16 +0300

    readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a Python script that, using this REST API, for a given 
+employee ID, returns information about his/her TODO list progress."""
+
+import json
+import requests
+import sys
+
+def get_data():
+    userId = sys.argv[1]
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/?id={userId}").json()
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos/?userId={}".format(userId)).json()
+    completed_tasks = 0
+    for task in tasks:
+        if task["completed"]:
+            completed_tasks += 1
+
+    print(f"Employee {user[0]['name']} is done with tasks ({completed_tasks}/{len(tasks)}):")
+    for task in tasks:
+        if task["completed"]:
+            print(f"\t{task['title']}")
+
+
+
+if __name__ == "__main__":
+    get_data()
