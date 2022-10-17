baeb48ba6ae447df377f8811b2a8b3319016e729
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-10-17 14:25:49 +0300

    int parsing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,18 +9,22 @@ import sys
 if __name__ == "__main__":
     """get data from api"""
     userId = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user = requests.get(f"{base_url}/users/?id={userId}").json()
-    tasks = requests.get(f"{base_url}/todos/?userId={userId}").json()
-    completed_tasks = 0
-    for task in tasks:
-        if task.get("completed"):
-            completed_tasks += 1
+    try:
+        userId = int(userId)
+        base_url = "https://jsonplaceholder.typicode.com"
+        user = requests.get(f"{base_url}/users/?id={userId}").json()
+        tasks = requests.get(f"{base_url}/todos/?userId={userId}").json()
+        completed_tasks = 0
+        for task in tasks:
+            if task.get("completed"):
+                completed_tasks += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(
-                user[0].get('name'), completed_tasks, len(tasks)),
-          file=sys.stdout)
-    for task in tasks:
-        if task["completed"]:
-            print(f"\t {task.get('title')}",
-                  file=sys.stdout)
+        print("Employee {} is done with tasks({}/{}):".format(
+                    user[0].get('name'), completed_tasks, len(tasks)),
+              file=sys.stdout)
+        for task in tasks:
+            if task["completed"]:
+                print(f"\t {task.get('title')}",
+                      file=sys.stdout)
+    except ValueError:
+        print("please enter a number")
