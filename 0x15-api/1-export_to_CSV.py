ff647ca08f53bf91a3b11b7ae40d841eebf8e141
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-10-17 15:13:15 +0300

    module docs correction

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""
-a Python script that, using this REST API, for a given
+"""a Python script that, using this REST API, for a given
+
 employee ID, returns information about his/her TODO list progress.
 """
 
